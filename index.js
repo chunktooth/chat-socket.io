dfc765be705c619d33cd0f65adc7f7a6249aa480
chunktooth <om.saetehn@gmail.com>
2018-05-23 11:55:49 -0600

    added 3 extra exercises

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+	io.emit('userConnected', `A new user, ${Date.now()}, has connected.`)
+
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    socket.broadcast.emit('chat message', `${msg.name}: ${msg.text}`);
   });
+
+	socket.join('chatroom');
+	io.to('chatroom').emit('Welcome aboard!');
+
+	socket.on('disconnect', function(msg) {
+		io.emit('userDisconnected', 'A user has disconnected');
+	});
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(3000, function(){
+  console.log('listening on *: 3000');
 });
+      
